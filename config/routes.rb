828c41bfff359a40254523a093104d862da5197a
Jonathan Rodriguez <jrguezg1@gmail.com>
2015-07-06 19:09:54 -0400

    Added seller page and navbar edit

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   resources :listings
   get 'pages/about'
-
   get 'pages/contact'
+  get 'seller' => "listings#seller"
 
   root 'listings#index'
+
 
 
 
