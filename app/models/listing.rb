784abaa350eb87022b0c350cf332321c2b421993
Jonathan Rodriguez <jrguezg1@gmail.com>
2015-07-09 17:25:42 -0400

    Enable Users to place orders

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -12,5 +12,6 @@ class Listing < ActiveRecord::Base
    validates :price, numericality:{ greater_than: 0 }
    validates :image, :attachment_presence => true
 
-   belongs_to :user 
+   belongs_to :user
+   has_many :orders
 end
